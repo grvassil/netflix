2db09f0e4874ae4ea640ae04fc95693697a54d69
grvassil <grvassil@gmail.com>
2019-07-30 22:10:15 +0300

    final version

--- js/main.js
+++ js/main.js
@@ -4,12 +4,18 @@ const tabContentItems = document.querySelectorAll(".tab-content-item");
 // Select tab conent item
 function selectItem(e) {
   removeBorder();
+  removeShow();
   // Add border to current tab
   this.classList.add("tab-border");
+  const tabContentItem = document.querySelector(`#${this.id}-content`);
+  tabContentItem.classList.add("show");
 }
 
 function removeBorder() {
   tabItems.forEach(item => item.classList.remove("tab-border"));
+}
+function removeShow() {
+  tabContentItems.forEach(item => item.classList.remove("show"));
 }
 
 // Listen for tab click
